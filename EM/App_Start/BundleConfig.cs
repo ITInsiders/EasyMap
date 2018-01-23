1b32b3107c141c8685e2d8da6dd9fc90970e4f89
Dmitry Vasiliev <ceh4top@gmail.com>
2018-01-23 23:43:07 +0300

    Подготовка 1 - Подключение и обновление библиотек - Удаление ненужного

--- EM/App_Start/BundleConfig.cs
+++ EM/App_Start/BundleConfig.cs
@@ -8,24 +8,31 @@ namespace EM
         // Дополнительные сведения об объединении см. на странице https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/signalr").Include(
+                        "~/Scripts/jquery.signalR-2.2.2.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
-            // готово к выпуску, используйте средство сборки по адресу https://modernizr.com, чтобы выбрать только необходимые тесты.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/CSS/Main").Include(
+                        "~/Assets/JS/Main.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+                        "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/JS/Main").Include(
+                        "~/Fonts/Font.css",
+                        "~/Assets/CSS/Main.css"));
         }
     }
 }
---
+++ EM/App_Start/SignalR.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EM.SignalR))]
+namespace EM
+{
+    public class SignalR
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
